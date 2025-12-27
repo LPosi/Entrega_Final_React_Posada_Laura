cb7f2464b9647e9373fcd64363a69f9e8722fa57
LPosi <lrposada@yahoo.com.ar>
2025-12-26 22:18:19 -0300

    Sigo revisando import, export, rutas, etc

--- src/App.jsx
+++ src/App.jsx
@@ -15,15 +15,15 @@ function App() {
         <Navbar />
         <main className="main-content">
           <Routes>
-            <Route path="/" element={<HomePage />} /> {/* Cambiar esta línea */}
+            <Route path="/" element={<Home />} />
             <Route path="/products" element={<ItemListContainer />} />
             <Route
               path="/category/:categoryId"
               element={<ItemListContainer />}
             />
             <Route path="/item/:itemId" element={<ItemDetailContainer />} />
-            <Route path="/cart" element={<CartPage />} />
-            <Route path="/checkout" element={<CheckoutPage />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/checkout" element={<Checkout />} />
             <Route path="*" element={<h1>404 - Página no encontrada</h1>} />
           </Routes>
         </main>
